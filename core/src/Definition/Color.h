69b26d3294d98f022ca9207359b0dbe8895e037e
Defisym <defisym@outlook.com>
2025-02-20 10:20:32 +0800

    `Score` as blade heart

--- core/src/Definition/Color.h
+++ core/src/Definition/Color.h
@@ -11,7 +11,8 @@ enum class Color :std::uint8_t {
     Green,
     Blue,
     Purple,
-    Any,   // live requirement only
-    All,   // blade heart only
-    Extra, // blade heart only
+    Any,   // live requirement only, any color is valid
+    All,   // blade heart only, select one color
+    Extra, // blade heart only, draw one more card
+    Score, // blade heart only, live score +1
 };
